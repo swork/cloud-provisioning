ac616a98c8748940119921abdc92deac5e155947
Steve Work <steve@work.renlabs.com>
2020-04-30 16:48:06 -0700

    Runtime requires Werkzeug

--- setup.py
+++ setup.py
@@ -6,28 +6,29 @@ import logging
 logging.basicConfig(datefmt='', level=os.environ.get('LEVEL', 'DEBUG'))
 logger = logging.getLogger(__name__)
 
-install_only_runtime = os.environ.get('INSTALL_ONLY_RUNTIME')
+INSTALL_ONLY_RUNTIME = os.environ.get('INSTALL_ONLY_RUNTIME')
 
 packages = []
 install_requires = []
 
-if install_only_runtime:
-    if install_only_runtime == 'renlabs.runtime.aws.wsgi':
+if INSTALL_ONLY_RUNTIME:
+    if INSTALL_ONLY_RUNTIME == 'renlabs.runtime.aws.wsgi':
         packages = [install_only_runtime]
+        install_requires = ['Werkzeug']
         logger.info('Installing only the AWS runtime')
     else:
         raise RuntimeError("INSTALL_ONLY_RUNTIME unknown package: ",
-                           install_only_runtime)
+                           INSTALL_ONLY_RUNTIME)
 else:
     packages = [
         'renlabs.provisioning.aws',
         'renlabs.runtime.aws.wsgi' ]
-    install_requires = ['boto3']
+    install_requires = ['boto3', 'Werkzeug']
     logger.info('Installing provisioning code; including all runtimes')
 
 setup(
     name='cloud-provisioning',
-    version='1.0.0rc0',
+    version='1.0.0rc1',
     description='Capture what I once knew about programmatic cloud-service setup',
     author='Steve Work',
     author_email='steve@work.renlabs.com',
